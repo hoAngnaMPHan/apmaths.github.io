bc77f339c6583ba3b36cf65610e042dfa9b6d5f1
APMatHS <153115073+APMatHS@users.noreply.github.com>
2023-12-09 18:31:35 +0700

    Delete css/ad.js

--- css/ad.js
+++
@@ -1,8 +0,0 @@
-<script type='text/javascript'>
-
-//<![CDATA[
-
-function generate(){var e,n=document.getElementById("downloadx"),t=document.getElementById("btnx"),a=document.getElementById("downloadx").href,l=30,d=document.createElement("span");n.parentNode.replaceChild(d,n),e=setInterval(function(){--l<0?(d.parentNode.replaceChild(n,d),clearInterval(e),window.location.replace(a),n.style.display="inline"):(d.innerHTML="<p style='font-family:Google Sans, sans-serif'>Vui lòng chờ "+l.toString()+" seconds...</p>",t.style.display="none")},1e3)}
-
-//]]>
-</script>
