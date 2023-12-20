e76cdc60dba57b618b4cf3e62a8fc230cb92425d
APMatHS <153115073+APMatHS@users.noreply.github.com>
2023-12-20 17:37:31 +0700

    Create MathJaxConfig.js

---
+++ assets/js/MathJaxConfig.js
@@ -0,0 +1,14 @@
+MathJax.Hub.Config({
+    tex2jax: {
+        inlineMath: [['$', '$'], ['\\(', '\\)']],
+        displayMath: [['$$', '$$'], ['\\[', '\\]']],
+        processEscapes: true,
+        processEnvironments: true,
+    },
+    "HTML-CSS": {
+        availableFonts: ["TeX"],
+        linebreaks: { automatic: true },
+        scale: 100,
+    },
+    displayAlign: 'center',
+});
